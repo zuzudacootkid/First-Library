a7ecf1900c40b8c5650131d5f14d0c7179bbe8a9
cenchyy <85932131+zuzudacootkid@users.noreply.github.com>
2022-05-24 00:18:05 +0000

    root = Tk() root.title("Virtual Library") root.mainloop()

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ class Starter:
         self.start_frame.grid(row=1, column=3)
 
 
+        self.heading_label = Label (self.start_frame, text = "Virtual library ct", bg="gray85", font=("montserrat", "12", "bold") )
+        self.heading_label.grid(row=1,column=3)       
+
+
 
 
 
@@ -15,5 +19,6 @@ class Starter:
 root = Tk()
 root.title("Virtual Library")
 root.mainloop()
+
 
 
