dbce37ef41777242b3293754c848d33fadbe2aeb
cenchyy <85932131+zuzudacootkid@users.noreply.github.com>
2022-05-24 00:06:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from tkinter import*
+
+class Starter:
+
+    def __init__(self, parent):
+
+        self.start_frame = Frame(parent, bg="pink", padx=150, pady=180)
+        self.start_frame.grid(row=1, column=3)
+
+
+
+
+
+
+
+
+root = Tk()
+root.title("Virtual Library")
+root.mainloop()
+
+
